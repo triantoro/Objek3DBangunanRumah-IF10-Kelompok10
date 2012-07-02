49aaf7a0fe2d917f511a1de8a9e3772efa0f5547
devidee <aiya.devi@yahoo.co.id>
2012-07-02 20:34:13 +0700

    Penambahan Pagar Atas dan pentilasi rumah
    Penambahan pentilasi rumah atas kiri dan kanan, serta membuat sebuah
    objek pagar

--- main.cpp
+++ main.cpp
@@ -701,6 +701,153 @@ void rumah(void) {
     glutSolidCube(5.0);
     glPopMatrix();        
      
+ 
+    
+//alis jedela atas (tengah1)
+    glPushMatrix();
+    glScaled(0.08, 0.017, 0.035);
+   glTranslatef(0, 119.5,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();  
+    
+    //alis jedela bawah (tengah1)
+    glPushMatrix();
+    glScaled(0.08, 0.017, 0.035);
+   glTranslatef(0, 40.0,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();     
+  
+    //alis jedela kiri (tengah1)
+    glPushMatrix();
+    glScaled(0.017,0.28, 0.035);
+   glTranslatef(-9.6, 4.8,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();   
+   
+    //alis jedela kanan (tengah1)
+    glPushMatrix();
+    glScaled(0.017,0.28, 0.035);
+   glTranslatef(9.5, 4.8,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix(); 
+
+//alis jedela atas (tengah2)
+    glPushMatrix();
+    glScaled(0.08, 0.017, 0.035);
+   glTranslatef(9, 119.5,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();  
+    
+    //alis jedela bawah (tengah2)
+    glPushMatrix();
+    glScaled(0.08, 0.017, 0.035);
+   glTranslatef(9, 40.0,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();     
+  
+    //alis jedela kiri (tengah2)
+    glPushMatrix();
+    glScaled(0.017,0.28, 0.035);
+   glTranslatef(33, 4.8,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();   
+   
+    //alis jedela kanan (tengah2)
+    glPushMatrix();
+    glScaled(0.017,0.28, 0.035);
+   glTranslatef(51.7, 4.8,128); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();     
+    
+    
+    
+   //alis tiang kiri atas orange
+    glPushMatrix();
+    glScaled(0.06, 0.037, 0.095);
+   glTranslatef(-41, 115,51.5); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();
+        
+   //alis tiang kiri bawah orange
+    glPushMatrix();
+    glScaled(0.06, 0.037, 0.095);
+   glTranslatef(-41, 80,51.5); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();     
+
+    //alis tiang kanan atas orange
+    glPushMatrix();
+    glScaled(0.06, 0.037, 0.095);
+   glTranslatef(41, 115,51.5); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();
+    
+   //alis tiang kanan bawah orange
+    glPushMatrix();
+    glScaled(0.06, 0.037, 0.095);
+   glTranslatef(41, 80,51.5); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();      
+
+    //orange 3 di tengah
+    glPushMatrix();
+    glScaled(0.017,0.33, 0.035);
+   glTranslatef(-16.6, 12,125); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();     
+
+    //orange 3 di tengah
+    glPushMatrix();
+    glScaled(0.017,0.33, 0.035);
+   glTranslatef(-6.6, 12,125); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();  
+    
+//orange 3 di tengah
+    glPushMatrix();
+    glScaled(0.017,0.33, 0.035);
+   glTranslatef(3.6, 12,125); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1.0000, 0.5252, 0.0157);
+    glutSolidCube(5.0);
+    glPopMatrix();  
+
+    //pagar atas 1
+    glPushMatrix();
+    glScaled(.88, 0.017, 0.017);
+   glTranslatef(-.01, 149,290); 
+   glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+     glColor3f(1,1,1);
+    glutSolidCube(5.0);
+    glPopMatrix(); 
      
 }
 
